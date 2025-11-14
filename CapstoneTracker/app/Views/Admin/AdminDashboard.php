4acc7441669b21340b6adbfc40fbe9d0b0e5157b
Rwoynie <rltiempo25@gmail.com>
2025-11-14 12:03:19 +0800

    file format note

--- CapstoneTracker/app/Views/Admin/AdminDashboard.php
+++ CapstoneTracker/app/Views/Admin/AdminDashboard.php
@@ -1117,6 +1117,7 @@ $displayUserData = [
                         <div class="upload-text">
                             <h3>Abstract File</h3>
                             <p>Supported files: PDF only</p>
+                            <p>(Include in File Name: _Abstract.pdf)</p>
                         </div>
                         <div class="browse-btn">Browse files</div>
                         <input type="file" class="file-input" id="abstractFileInput" name="abstract_file" accept=".pdf">
@@ -1130,6 +1131,7 @@ $displayUserData = [
                         <div class="upload-text">
                             <h3>Thesis File</h3>
                             <p>Supported files: PDF only</p>
+                            <p>(Include in File Name: _Thesis.pdf)</p>
                         </div>
                         <div class="browse-btn">Browse files</div>
                         <input type="file" class="file-input" id="thesisFileInput" name="thesis_file" accept=".pdf">
